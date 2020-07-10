93b28c83ae7fafb452c2da9d5cdd042fdd9e8bd7
Dyrr <dyrr73@gmail.com>
2020-07-11 00:31:57 +0200

    Ordinamento codice

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
 <?php
+/**
+ *  @file       index.php
+ *  
+ *  @brief      File index del GDRCD
+ *  
+ *  @version    5.6.0
+ *  @date       1/07/2020
+ *  
+ *  @author     Davide 'Dyrr' Grandi
+ */    
     define('ROOT', __DIR__);
 
     //Includo i parametri, la configurazione, la lingua e le funzioni
     require_once ROOT . '/includes/required.php';
 
-    $query =    "SELECT 
-                    COUNT(nome) AS online 
-                FROM personaggio 
-                WHERE 
-                        ora_entrata > ora_uscita 
-                    AND DATE_ADD(ultimo_refresh, INTERVAL 4 MINUTE) > NOW()";   
+    $query = "SELECT 
+                 COUNT(nome) AS online 
+             FROM personaggio 
+             WHERE 
+                     ora_entrata > ora_uscita 
+                 AND DATE_ADD(ultimo_refresh, INTERVAL 4 MINUTE) > NOW()";   
     $users = gdrcd_query($query);
 
     //definizione della sezione da caricare
