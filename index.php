d6c852e083094f079782ae8c4380c0ebdf94599b
Dyrr <dyrr73@gmail.com>
2020-07-11 00:24:06 +0200

    Ordinamento codice

--- index.php
+++ index.php
@@ -1,26 +1,30 @@
 <?php
-	define('ROOT', __DIR__);
+    define('ROOT', __DIR__);
 
-	//Includo i parametri, la configurazione, la lingua e le funzioni
-	require_once ROOT . '/includes/required.php';
+    //Includo i parametri, la configurazione, la lingua e le funzioni
+    require_once ROOT . '/includes/required.php';
 
-    $users = gdrcd_query("SELECT COUNT(nome) AS online FROM personaggio WHERE ora_entrata > ora_uscita AND DATE_ADD(ultimo_refresh, INTERVAL 4 MINUTE) > NOW()");
+    $query =    "SELECT 
+                    COUNT(nome) AS online 
+                FROM personaggio 
+                WHERE 
+                        ora_entrata > ora_uscita 
+                    AND DATE_ADD(ultimo_refresh, INTERVAL 4 MINUTE) > NOW()";   
+    $users = gdrcd_query($query);
 
     //definizione della sezione da caricare
-	$page = ( ! empty($_GET['page'])) ? gdrcd_filter('include', $_GET['page']) : 'index';
+    $page = ( ! empty($_GET['page'])) ? gdrcd_filter('include', $_GET['page']) : 'index';
 
     //include la parte con la logica della pagina
-	require modulo\file('home/' . $page);
-		
-	//require template\file('_base');
+    require modulo\file('home/' . $page);
 
-	gdrcd_close_connection($handleDBConnection);
+    gdrcd_close_connection($handleDBConnection);
 
     unset($MESSAGE);
     unset($PARAMETERS);
-	
-	echo $OUT['header'];	
-	echo $OUT['layout_top'];	
-	echo $OUT['content'];	
-	echo $OUT['layout_bottom'];	    
-	echo $OUT['footer'];
+
+    echo $OUT['header'];    
+    echo $OUT['layout_top'];    
+    echo $OUT['content'];   
+    echo $OUT['layout_bottom'];     
+    echo $OUT['footer'];
