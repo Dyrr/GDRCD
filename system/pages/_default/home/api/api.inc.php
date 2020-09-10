adbaa104bee6dd09674065f909c97ed8b9eafa24
Dyrr <dyrr73@gmail.com>
2020-09-10 21:40:25 +0200

    spazi

--- system/pages/_default/home/api/api.inc.php
+++ system/pages/_default/home/api/api.inc.php
@@ -15,13 +15,13 @@
     $view = $op;
 
     \functions\load('core/api');
-	
-	//ESEGUE IL SET DI ISTRUZIONI IN BASE ALL'OPERAZIONE RICHIESTA
+    
+    //ESEGUE IL SET DI ISTRUZIONI IN BASE ALL'OPERAZIONE RICHIESTA
     switch(strtoupper($op)) {
 
  
-		
-		default :
+        
+        default :
         
         break;
 
@@ -31,75 +31,75 @@
     
     //ESEGUE IL SET DI ISTRUZIONI IN BASE ALLA VIEW RICHIESTA       
     switch(strtoupper($view)) {
-		
-		case 'DBSTRUCTURE' :
-		
-			if(   isset($_SESSION['id']) 
-			   && $_SESSION['permessi'] >= SUPERUSER) {
-			
+        
+        case 'DBSTRUCTURE' :
+        
+            if(   isset($_SESSION['id']) 
+               && $_SESSION['permessi'] >= SUPERUSER) {
+            
 
-				\functions\load('core/install'); 
-				$TAG['page'] = \install\dbStructure();
-				
-			} else {
-				   
-				\functions\load('core/install'); 
-				$TAG['page'] = \install\dbStructure();				
-				//$TAG['page'] = 'Furbacchione!!!';
-				   
-			}
+                \functions\load('core/install'); 
+                $TAG['page'] = \install\dbStructure();
+                
+            } else {
+                   
+                \functions\load('core/install'); 
+                $TAG['page'] = \install\dbStructure();              
+                //$TAG['page'] = 'Furbacchione!!!';
+                   
+            }
  
-			
-		
-		
-		break;
-		
-		case 'VERSION' : 
+            
+        
+        
+        break;
+        
+        case 'VERSION' : 
 
-			$TAG['page']['version'] = $PARAMETERS['version'];
-			$TAG['page']['requisiti'] = \api\requisiti();
-		
-		break;
-		
-		default :
-		
+            $TAG['page']['version'] = $PARAMETERS['version'];
+            $TAG['page']['requisiti'] = \api\requisiti();
+        
+        break;
+        
+        default :
+        
 
-			$TAG['page']['intro'] = "Pagina di default delle API del GDRCD 5.6";
-			$TAG['page']['comandi'][] = array(
-				'operazione' => 'pg',
-				'parametri' => array(
-					0 => array(
-						'parametro' => 'nome',
-						'tipo' => 'string',
-						'descrizione' => 'nome del personaggio di cui si vogliono vedere i dati'
-					),
-					1 => array(
-						'parametro' => 'key',
-						'tipo' => 'string',
-						'descrizione' => 'Chiave delle api utilizzata dal personaggio'
-					),
-					2 => array(
-						'parametro' => 'format',
-						'tipo' => 'string',
-						'valori' => 'json (default),html',
-						'opzionale' => 'si',
-						'descrizione' => 'Formato in cui restituire i dati'
-					),
-				
-				),
-				
-				'esempio' => 'api.php?op=pg&nome=Dyrr&key=ahf24y2',
-				'descrizione' => 'Questo comando delle api restituisce i dati del pg scelto per la chiave selezionata. In base alla chiave selezionata per lo stesso pg potrebbero essere disponibili diverse categorie di dati'
-			
-			);
-			$TAG['page']['comandi'][] = array(
-				'operazione' => 'version',
-				'esempio' => 'api.php?op=version',
-				'descrizione' => 'Questo comando delle api restituisce la versione dell\'Open Source installata assieme ad altri dati'
-			
-			);		
-		break;
+            $TAG['page']['intro'] = "Pagina di default delle API del GDRCD 5.6";
+            $TAG['page']['comandi'][] = array(
+                'operazione' => 'pg',
+                'parametri' => array(
+                    0 => array(
+                        'parametro' => 'nome',
+                        'tipo' => 'string',
+                        'descrizione' => 'nome del personaggio di cui si vogliono vedere i dati'
+                    ),
+                    1 => array(
+                        'parametro' => 'key',
+                        'tipo' => 'string',
+                        'descrizione' => 'Chiave delle api utilizzata dal personaggio'
+                    ),
+                    2 => array(
+                        'parametro' => 'format',
+                        'tipo' => 'string',
+                        'valori' => 'json (default),html',
+                        'opzionale' => 'si',
+                        'descrizione' => 'Formato in cui restituire i dati'
+                    ),
+                
+                ),
+                
+                'esempio' => 'api.php?op=pg&nome=Dyrr&key=ahf24y2',
+                'descrizione' => 'Questo comando delle api restituisce i dati del pg scelto per la chiave selezionata. In base alla chiave selezionata per lo stesso pg potrebbero essere disponibili diverse categorie di dati'
+            
+            );
+            $TAG['page']['comandi'][] = array(
+                'operazione' => 'version',
+                'esempio' => 'api.php?op=version',
+                'descrizione' => 'Questo comando delle api restituisce la versione dell\'Open Source installata assieme ad altri dati'
+            
+            );      
+        break;
     
-	}
+    }
 
-	\template\mode('json');
+    \template\mode('json');
